dda5d9db2fe1ed805eb3b2c8324d8318b3e02fb8
kviet91 <kviet91.cdp@gmail.com>
2020-11-25 01:07:22 +0700

    chua_sua

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,13 +8,40 @@ use App\Models\Menu;
 class HomeController extends Controller
 {
     public function index() {
-        $menus0 = Menu::where('type','header')->where('parent_id',0)->orderby('order')->get();
-        $menus1 = Menu::where('type','header')->where('parent_id','=',1)->orderby('order')->get();
-        $menus2 = Menu::where('type','header')->where('parent_id','=',2)->orderby('order')->get();
+        $menus0 = Menu::where('type','header-main')->orderby('order')->get()->toArray();
+        $arr_menus = [];
+        foreach($menus0 as $menu0){
+            $arr_menus[$menu0['parent_id']][] = $menu0;
+
+        }
+
+        //list cha
+        echo '<ul>';
+            foreach($arr_menus[0] as $mp) {
+                echo '<li>'
+                . $mp['name']. '--' .$mp['id'];
+                if(isset($arr_menus[$mp['id']])) 
+                {
+                    echo '<ul>';
+                    foreach($arr_menus[$mp['id']] as $mp_c) 
+                    {
+                        echo'<li>' . $mp_c['name'] . '--' . $mp_c['id'];
+
+                        if(isset($arr_menus[$mp['id']]))
+
+
+                        echo '</li>';
+                    }
+                    echo '</ul>';
+                }
+                echo '</li>';
+            }
+        echo '</ul>';
+        dd($arr_menus);
+
+
 
         view()->share('menus0', $menus0);
-        view()->share('menus1', $menus1);
-        view()->share('menus2', $menus2);
 
         return view('pages.index');
     }
